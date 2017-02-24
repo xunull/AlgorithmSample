4fdb1665b5f56e9b4afd71221211f1e604772b1b
xunull <todogo@vip.163.com>
2017-02-24 23:00:37 +0800

    add some snippets

---
+++ basic/python/埃拉托色尼筛选法/sieve.py
@@ -0,0 +1,36 @@
+'''
+素数筛选
+'''
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):
+    return lambda x: x % n >0
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+
+
+for n in primes():
+    if n < 1000000:
+        print(n)
+    else:
+        break
+
+
+
+
+if __name__ == '__main__':
+    pass
+else:
+    pass
+
